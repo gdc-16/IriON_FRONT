d4b75f3cac4fe56273e0be42b4348af557120603
jejecrunch <gkswlgp456@gmail.com>
2022-06-25 23:04:20 +0900

    temp: AWS 테스트

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/common/Header/Header';
+import RowSlider from '../../components/recommend/rowSlider';
 
 export function Main() {
-  return <div><Header /></div>
+  return <div><Header /><RowSlider /></div>
 }
