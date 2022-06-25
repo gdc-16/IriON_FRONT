98f84f1d2d211b060a851b8593861dd14235cecf
jejecrunch <gkswlgp456@gmail.com>
2022-06-26 01:19:46 +0900

    add: 메인 화면 중 마지막 파트까지 틀 구현 완료

---
+++ src/components/moreRecommend/List.js
@@ -0,0 +1,26 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Grid from './grid';
+import '../../assets/css/moreRecommend.css'
+
+export default function List(){
+    const items = [{day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'},{day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}, {day: 6, name: '치와와', age: 3, gender:'여', carehouse: '서양보호소'}]
+
+    return (
+        <section className="more-recommend">
+                <h2 className="more-recommend-title">다른 아이들도 있어요!</h2>
+                <div className="sort-button">
+                    <button className="button">오늘 들어온 아이</button>
+                    <button className="button">보호소 터줏대감</button>
+                </div>
+                <ul className="grid">
+                    {items.map((item, key)=>{
+                        return (
+                            <li className="grid-items" key={key}>
+                                <Grid data={item} />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+    )
+}
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Login() {
-  return (<div>로그인</div>)
+  return (<div className='login_area'>
+    <form>
+      <input type="text" placeholder='id' />
+      <input type="password" placeholder='password' />
+      <input type="submit" />
+    </form>
+    <Link to="/signup">회원가입</Link>
+    </div>)
 }
