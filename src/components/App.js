720f6233bfea2c93b1b0440812842747d3c7aee8
jejecrunch <gkswlgp456@gmail.com>
2022-06-25 23:04:20 +0900

    temp: AWS 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Main, Follow, Alert } from '../pages/index';
+import { Login } from '../pages/Login/Login';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Main />} />  
         <Route path="/follow" element={<Follow />} />
         <Route path="/alert" element={<Alert />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
